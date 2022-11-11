221a794e7a2615dffe2b45c531c0194b71bbe778
da15q9 <emese.borbely@stud.uni-corvinus.hu>
2022-11-11 11:06:49 +0100

    problémamegoldva

--- hetedikke/hetedikke/Program.cs
+++ hetedikke/hetedikke/Program.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using hetedikke;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace hetedikke
+namespace week05
 {
     static class Program
     {
@@ -12,8 +13,8 @@ namespace hetedikke
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main();
-        f{
+        static void Main()
+        {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());

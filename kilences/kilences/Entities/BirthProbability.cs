1837d198b67e3916a0d5918c67e141777dbf7b40
da15q9 <emese.borbely@stud.uni-corvinus.hu>
2022-12-02 10:04:20 +0100

    osztályok

---
+++ kilences/kilences/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kilences.Entities
+{
+    public class BirthProbability
+    {
+        //kor, gyermekek száma és a valószínűség
+        public int Kor { get; set; }
+        public int Gyermekek { get; set; }
+        public double GyValószínűség { get; set; }
+    }
+}
---
+++ kilences/kilences/Entities/DeathProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kilences.Entities
+{
+    class DeathProbability
+    {
+        //nem, kor és a halálozási valószínűség
+        public Gender Gender { get; set; }
+        public int Kor { get; set; }
+        public double HaValószínűség { get; set; }
+    }
+}
